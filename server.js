e4a5e89884bddf99e9fe4d2b7fa97e09faa9fdd4
BuildTools <ayoubzitouni315@gmail.com>
2022-11-21 18:05:50 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express=require("express")
+const app=express()
+const pug=require('pug')
+app.set('view engine','pug')
+app.set('views','./views')
+app.use(timer)
+
+app.get('/',(req,res)=>{
+    res.status(200).render("home")
+})
+app.get('/our-services',(req,res)=>{
+    res.status(200).render("services")
+})
+app.get('/contact',(req,res)=>{
+    res.status(200).render("contact")
+})
+
+
+
+function timer(req,res,next){
+    const timeNow=new Date()
+    const Time=timeNow.getHours()
+    if(Time<17 && Time>9 ){
+        
+    }else{
+        res.status(200).render("error")
+    }
+    next()
+}
+app.listen(3000)
